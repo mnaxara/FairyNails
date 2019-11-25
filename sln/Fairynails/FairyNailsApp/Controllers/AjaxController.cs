09c47290ac36d8ab32e4b4f39ae18eb221e84c2f
mnaxara <m.naxara@hotmail.com>
2019-11-25 11:36:01 +0100

    Add ajaxrequest to change calendar month

---
+++ sln/Fairynails/FairyNailsApp/Controllers/AjaxController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FairyNails.Service.PrestationServices;
+using FairyNails.Service.RendezVousServices;
+using FairyNailsApp.Models.RendezVous;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FairyNailsApp.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly IRendezVousService _rendezVousService;
+
+        public AjaxController(IRendezVousService rendezVousService, IPrestationService prestationService)
+        {
+            this._rendezVousService = rendezVousService;
+        }
+
+        [HttpPost]
+        public IActionResult ChangeMonth(Int32 month, Int32 year)
+        {
+            RendezVousViewModel rendezVous = new RendezVousViewModel()
+            {
+                DateCodeRendezVousTaken = _rendezVousService.GetTakenRendezVousTimeCode(),
+                FirstDayOfMonth = new DateTime(year, month, 1)
+            };
+
+            return PartialView(rendezVous);
+        }
+    }
+}
